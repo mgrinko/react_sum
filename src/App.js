6ce4f0c1617356175f6d71b40c11113abaefe257
mgrinko <glukym@gmail.com>
2021-06-16 10:20:31 +0300

    optimizations were added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 import './App.scss';
@@ -6,17 +6,34 @@ import * as todosAPI from './api/todos';
 import { TodoForm } from './components/TodoForm';
 import { TodoList } from './components/TodoList';
 
+const debounce = (f, delay) => {
+  let timerId;
+
+  return (...args) => {
+    if (timerId) {
+      clearTimeout(timerId);
+    }
+
+    timerId = setTimeout(f, delay, ...args);
+  };
+}
+
 const App = () => {
   const [todos, setTodos] = useState([]);
+  const [query, setQuery] = useState('');
+  const [appliedQuery, setAppliedQuery] = useState('');
+
+  const apply = useCallback(
+    debounce(setAppliedQuery, 1000),
+    []
+  );
 
   const loadTodos = () => {
     todosAPI.getTodos()
       .then(setTodos);
   };
 
-  useEffect(() => {
-    loadTodos();
-  }, []);
+  useEffect(loadTodos, []);
 
   const addTodo = (todoData) => {
     // const newTodo = { ...todoData, id: uuidv4() };
@@ -25,14 +42,13 @@ const App = () => {
     todosAPI.addTodo(todoData)
       .then(loadTodos)
   };
-  const deleteTodo = (todoId) => {
-    // const filtered = todos.filter(todo => todo.id !== todoId);
-    // setTodos(filtered);
+  const deleteTodo = useCallback((todoId) => {
+    // setTodos(current => current.filter(todo => todo.id !== todoId));
 
     todosAPI.deleteTodo(todoId)
       .then(loadTodos);
-  };
-  const updateTodo = async (updatedTodo) => {
+  }, []);
+  const updateTodo = useCallback(async (updatedTodo) => {
     // const copy = [...todos];
     // const index = todos.findIndex(todo => todo.id === updatedTodo.id);
     //
@@ -41,13 +57,25 @@ const App = () => {
 
     await todosAPI.updateTodo(updatedTodo)
     loadTodos();
+  }, []);
+
+  const handleQueryChange = event => {
+    setQuery(event.target.value);
+    apply(event.target.value);
   };
 
+  const visibleTodos = useMemo(() => {
+    return todos.filter(
+      todo => todo.title.includes(appliedQuery)
+    );
+  }, [appliedQuery, todos]);
+
   return <>
-    Add todo:
-    <TodoForm saveTodo={addTodo} />
+    <input type="text" value={query} onChange={handleQueryChange}/>
+    {query}
+
     <TodoList
-      todos={todos}
+      todos={visibleTodos}
       deleteTodo={deleteTodo}
       updateTodo={updateTodo}
     />
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import { Todo } from './Todo';
 
-export const TodoList = ({ todos, deleteTodo, updateTodo }) => {
-  return (
-    <ul className="TodoList">
-      {todos.map(todo => (
-        <li key={todo.id}>
-          <Todo
-            todo={todo}
-            onDelete={() => deleteTodo(todo.id)}
-            updateTodo={updateTodo}
-          />
-        </li>
-      ))}
-    </ul>
-  );
-};
+export const TodoList = React.memo(
+  ({ todos, deleteTodo, updateTodo }) => {
+    console.log('Rendering TodoList');
+
+    return (
+      <ul className="TodoList">
+        {todos.map(todo => (
+          <li key={todo.id}>
+            <Todo
+              todo={todo}
+              onDelete={() => deleteTodo(todo.id)}
+              updateTodo={updateTodo}
+            />
+          </li>
+        ))}
+      </ul>
+    );
+  }
+);
 
 
